f01f32fd2f3360dd81e2e7cc3569de15c02be376
Matt Phillips <code@mattman.dev>
2022-09-23 16:07:03 -0700

    added basic HTTP method handling and use of stdlib constants

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,17 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+		return
+	}
+
 	w.Write([]byte("Hello from Snippetbox!"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display snippet with Id %d\n", id)
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// limit to POST calls
+	if r.Method != http.MethodPost {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("Create a snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Printf("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
