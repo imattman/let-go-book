9047d3d96e748e86903b9906d4f964fc634f4bef
Matt Phillips <code@mattman.dev>
2022-10-15 11:29:38 -0700

    added middleware funcs for logging and HTTP headers

--- snippetbox/cmd/web/routes.go
+++ snippetbox/cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 
 	mux := http.NewServeMux()
 
@@ -14,5 +14,9 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	return app.logRequest(
+		secureHeaders(
+			mux,
+		),
+	)
 }
